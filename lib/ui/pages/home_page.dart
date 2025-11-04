95ff50083032c4f1ce6e1658fc798f0e4addaedd
minhtridaoit <mtrihocit05@gmail.com>
2025-11-04 23:12:32 +0700

    edit banner and add directory ui

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -20,9 +20,12 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // ignore: deprecated_member_use
+      backgroundColor: Colors.grey.withOpacity(0.2),
       appBar: AppBar(
         toolbarHeight: 100,
-        backgroundColor: Colors.white,
+        // ignore: deprecated_member_use
+        backgroundColor: Colors.grey.withOpacity(0.2),
         title: SafeArea(
           child: Column(children: [_buildAddress(), _buildSearchBar()]),
         ),
@@ -33,6 +36,8 @@ class _HomePageState extends State<HomePage> {
             _bannerSlide(),
             const SizedBox(height: 8),
             _navigationController(),
+            const SizedBox(height: 25),
+            _buildDirectory(),
           ],
         ),
       ),
@@ -68,8 +73,9 @@ class _HomePageState extends State<HomePage> {
               borderRadius: BorderRadius.circular(12),
               child: Image.asset(
                 img,
+                height: 150,
                 fit: BoxFit.cover,
-                width: double.infinity,
+                width: 325,
               ),
             );
           }).toList(),
@@ -117,6 +123,60 @@ class _HomePageState extends State<HomePage> {
               ),
             );
           }).toList(),
+    );
+  }
+
+  Widget _buildDirectory() {
+    final categories = [
+      {"icon": "assets/iconDirectory/burger_icon.png", "label": "Burger"},
+      {"icon": "assets/iconDirectory/pizza_icon.png", "label": "Pizza"},
+      {"icon": "assets/iconDirectory/sandwich_icon.png", "label": "sandwich"},
+      {"icon": "assets/iconDirectory/burger_icon.png", "label": "Burger"},
+      {"icon": "assets/iconDirectory/burger_icon.png", "label": "Burger"},
+    ];
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal, //cuon ngang
+      child: Row(
+        children:
+            categories.asMap().entries.map((entry) {
+              int index = entry.key;
+              var item = entry.value;
+              return _categoryItem(
+                image: item["icon"]!,
+                label: item["label"]!,
+                isActive: index == 0,
+              );
+            }).toList(),
+      ),
+    );
+  }
+
+  Widget _categoryItem({
+    required String image,
+    required String label,
+    required bool isActive,
+  }) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10),
+      width: 126,
+      height: 43,
+      decoration: BoxDecoration(
+        color: Colors.pink,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Image.asset(image, height: 24, width: 24, fit: BoxFit.contain),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 18,
+              color: isActive ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
